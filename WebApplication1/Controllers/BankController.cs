03221e966bf738f718f5fd6b4627cf2118253237
83336 <halshamlan@yahoo.com>
2024-04-22 08:45:37 +0300

    Entity framework

--- WebApplication1/Controllers/BankController.cs
+++ WebApplication1/Controllers/BankController.cs
@@ -5,16 +5,18 @@ namespace WebApplication1.Controllers
 {
     public class BankController : Controller
     {
+
+
         private static List<BankBranch> bankBranches = new List<BankBranch>
         {
                 new BankBranch { Id = 1, Name = "kfh", LocationURL = "jjhshshs", LocationName = "kwt", BranchManager = "sara", EmployeeCount = 23 },
                 new BankBranch {Id = 2, Name = "nbk", LocationURL = "ggahah", LocationName = "sura", BranchManager = "haya", EmployeeCount = 45 }
         };
-public IActionResult Index()
+        public IActionResult Index()
         {
-           
-            return View(bankBranches);
-     
+            var context = new BankContext();
+            return View(context.BankBranches.ToList());
+
         }
         [HttpGet]
         public IActionResult Create()
@@ -26,26 +28,80 @@ public IActionResult Index()
         {
             if (ModelState.IsValid)
             {
-                var name = model.Name;
-                var locationURl = model.LocationURL;
-                var locationName = model.LocationName;
-                var BranchManager = model.BranchManager;
-                var employeeCount = model.EmployeeCout;
-                return RedirectToAction("Index");
+                using (var context = new BankContext())
+                {
+                    var bank = new BankBranch();
+                    var name = model.Name;
+                    var locationURl = model.LocationURL;
+                    var locationName = model.LocationName;
+                    var BranchManager = model.BranchManager;
+                    var employeeCount = model.EmployeeCout;
+                    bank.Name = name; ;
+                    bank.LocationURL = locationURl;
+                    bank.LocationName = locationName;
+                    bank.BranchManager = BranchManager;
+                    bank.EmployeeCount = employeeCount;
+                    context.BankBranches.Add(bank);
+                    context.SaveChanges();
+                }
+
             }
-            else
-            {
-                return View();
-            }
+            return RedirectToAction("Index");
         }
         public IActionResult Details(int id)
         {
-            var bankBranch = bankBranches.FirstOrDefault(b => b.Id == id);
-            if (bankBranch == null)
+            using (var context = new BankContext())
             {
-                return RedirectToAction("Index");
+                var bankBranch = bankBranches.FirstOrDefault(b => b.Id == id);
+                if (bankBranch == null)
+                {
+                    return NotFound();
+                }
+                return View(bankBranch);
             }
-            return View(bankBranch);
         }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            using (var context = new BankContext())
+            {
+                var branch = context.BankBranches.Find(id);
+                if (branch == null)
+                {
+                    return RedirectToAction("Index");
+                }
+                var form = new NewBranchForm();
+                form.LocationURL = branch.LocationURL;
+                form.LocationName = branch.LocationName;
+                form.BranchManager = branch.BranchManager;
+                form.Name = branch.Name;
+                form.EmployeeCout = branch.EmployeeCount;
+                return View(form);
+
+            }
+
+        }
+        [HttpPost]
+        public IActionResult Edit(int id, NewBranchForm newBranchForm) 
+        { 
+        using (var context = new BankContext())
+            {
+                var branch = context.BankBranches.Find(id);
+                if(branch == null)
+                {
+                    branch.Name = newBranchForm.Name;
+                    branch.LocationURL = newBranchForm.LocationURL;
+                    branch.LocationName = newBranchForm.LocationName;
+                    branch.BranchManager = newBranchForm.BranchManager;
+                    branch.EmployeeCount = newBranchForm.EmployeeCout;
+                    context.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View();
+            }
+                }
     }
 }
+
+
---
+++ WebApplication1/Models/BankContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class BankContext : DbContext
+    {
+        public DbSet<BankBranch> BankBranches { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=bank.db");
+        }
+    }
+
+}
